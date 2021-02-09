acafc17f0b98781a7d9ddadbc22d75e0c6e83f24
Wilker <wilker_a@hotmail.com>
2021-02-08 21:29:22 -0300

    atualizado arquivo header.blade.php

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -39,55 +39,49 @@
                     <div class="d-flex justify-content-between">
                         <div class="top-menu d-flex align-items-center">
                             <button type="button" class="btn-icon mobile-nav-toggle d-lg-none"><span></span></button>
-                            <div class="header-search">
-                                <div class="input-group">
-                                    <span class="input-group-addon search-close"><i class="ik ik-x"></i></span>
-                                    <input type="text" class="form-control">
-                                    <span class="input-group-addon search-btn"><i class="ik ik-search"></i></span>
-                                </div>
-                            </div>
+                           
                             <button type="button" id="navbar-fullscreen" class="nav-link"><i class="ik ik-maximize"></i></button>
                         </div>
                         <div class="top-menu d-flex align-items-center">
                             <div class="dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="notiDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="ik ik-bell"></i><span class="badge bg-danger">3</span></a>
+                                
                                 <div class="dropdown-menu dropdown-menu-right notification-dropdown" aria-labelledby="notiDropdown">
-                                    <h4 class="header">Notifications</h4>
-                                    <div class="notifications-wrap">
-                                        <a href="#" class="media">
-                                            <span class="d-flex">
-                                                <i class="ik ik-check"></i>
-                                            </span>
-                                            <span class="media-body">
-                                                <span class="heading-font-family media-heading">Invitation accepted</span>
-                                                <span class="media-content">Your have been Invited ...</span>
-                                            </span>
-                                        </a>
-                                        <a href="#" class="media">
-                                            <span class="d-flex">
-                                                <img src="{{asset('template/img/users/1.jpg')}}" class="rounded-circle" alt="">
-                                            </span>
-                                            <span class="media-body">
-                                                <span class="heading-font-family media-heading">Steve Smith</span>
-                                                <span class="media-content">I slowly updated projects</span>
-                                            </span>
-                                        </a>
-                                        <a href="#" class="media">
-                                            <span class="d-flex">
-                                                <i class="ik ik-calendar"></i>
-                                            </span>
-                                            <span class="media-body">
-                                                <span class="heading-font-family media-heading">To Do</span>
-                                                <span class="media-content">Meeting with Nathan on Friday 8 AM ...</span>
-                                            </span>
-                                        </a>
+                                        <h4 class="header">Notifications</h4>
+                                        <div class="notifications-wrap">
+                                            <a href="#" class="media">
+                                                <span class="d-flex">
+                                                    <i class="ik ik-check"></i>
+                                                </span>
+                                                <span class="media-body">
+                                                    <span class="heading-font-family media-heading">Invitation accepted</span>
+                                                    <span class="media-content">Your have been Invited ...</span>
+                                                </span>
+                                            </a>
+                                            <a href="#" class="media">
+                                                <span class="d-flex">
+                                                    <img src="{{asset('template/img/users/1.jpg')}}" class="rounded-circle" alt="">
+                                                </span>
+                                                <span class="media-body">
+                                                    <span class="heading-font-family media-heading">Steve Smith</span>
+                                                    <span class="media-content">I slowly updated projects</span>
+                                                </span>
+                                            </a>
+                                            <a href="#" class="media">
+                                                <span class="d-flex">
+                                                    <i class="ik ik-calendar"></i>
+                                                </span>
+                                                <span class="media-body">
+                                                    <span class="heading-font-family media-heading">To Do</span>
+                                                    <span class="media-content">Meeting with Nathan on Friday 8 AM ...</span>
+                                                </span>
+                                            </a>
+                                        </div>
+                                        <div class="footer"><a href="javascript:void(0);">See all activity</a></div>
                                     </div>
-                                    <div class="footer"><a href="javascript:void(0);">See all activity</a></div>
                                 </div>
-                            </div>
-                            <button type="button" class="nav-link ml-10 right-sidebar-toggle"><i class="ik ik-message-square"></i><span class="badge bg-success">3</span></button>
+                            <!--<button type="button" class="nav-link ml-10 right-sidebar-toggle"><i class="ik ik-message-square"></i><span class="badge bg-success">3</span></button>-->
                             <div class="dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="menuDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="ik ik-plus"></i></a>
+                               <!-- <a class="nav-link dropdown-toggle" href="#" id="menuDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="ik ik-plus"></i></a>-->
                                 <div class="dropdown-menu dropdown-menu-right menu-grid" aria-labelledby="menuDropdown">
                                     <a class="dropdown-item" href="#" data-toggle="tooltip" data-placement="top" title="Dashboard"><i class="ik ik-bar-chart-2"></i></a>
                                     <a class="dropdown-item" href="#" data-toggle="tooltip" data-placement="top" title="Message"><i class="ik ik-mail"></i></a>
