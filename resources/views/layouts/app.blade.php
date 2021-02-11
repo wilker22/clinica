c595aa8ee38736a034bb001a031602686c72a750
Wilker <wilker_a@hotmail.com>
2021-02-11 17:21:45 -0300

    datepicker frontend traduzido

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -108,9 +108,17 @@
         $( function() {
             $("#datepicker").datepicker({
                 dateFormat: "dd-mm-yy",
-                showButtonPanel: true,
+                showButtonPanel: false,
                 numberOfMonths: 2,
-                minDate: dateToday
+                minDate: dateToday,
+                locale: "pt-BR",
+                dayNames: ['Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado'],
+                dayNamesMin: ['D','S','T','Q','Q','S','S','D'],
+                dayNamesShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sáb','Dom'],
+                monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+                monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez'],
+                nextText: 'Próximo',
+                prevText: 'Anterior'
             });
         });
     </script>
